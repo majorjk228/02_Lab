b0fe071df5448b87c551f57d318c9530a89a4052
sana-love-cs sana-love-cs <sana-ivanov1@yandex.ru>
2023-01-14 02:23:55 +0500

    UPD FOR RUS

--- ConsoleApplication1/Lab2_main.cpp
+++ ConsoleApplication1/Lab2_main.cpp
@@ -30,8 +30,7 @@ void print_tovar(Route t)
 }
 void main()
 {
-	SetConsoleCP(1251);         // Ввод с консоли в кодировке 1251 для русской локализации.
-	SetConsoleOutputCP(1251);   // Вывод на консоль в кодировке 1251 для русской локализации.
+	setlocale(LC_ALL, "Russian");
 
 	//конструктор без параметров
 	Route t1;
